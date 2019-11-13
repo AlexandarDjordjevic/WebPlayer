3f11ad0a06e2b160a0bbe1b4f9ad07b447132a94
Aleksandar <aleksandar.djordjevic@novelic.com>
2019-11-13 16:59:08 +0100

    visual improvments

--- player.py
+++ player.py
@@ -11,23 +11,43 @@ from bluetooth import *
 class Player:
     def __init__(self):
         self.instance = vlc.Instance()
-        self.media = vlc.MediaPlayer()
+        self.media_player = self.instance.media_player_new()
+        self.media = 0
         self.state = 0
         self.playlist = ""
         self.url = ""
+        self.station_name = ""
+        self.current_track = ""
         self.thread = threading.Thread(target=self.worker, args=())
 
     def worker(self):   
         while True:
             if self.state == 0: #idle
-                time.sleep(1)
+                if self.media:
+                    self.media.parse()
+                    if self.media.get_meta(0):
+                        self.station_name = self.media.get_meta(0)
+                    else:
+                        self.station_name = " "
+                    if self.media.get_meta(12):
+                        self.current_track = self.media.get_meta(12)
+                    else:
+                        self.current_track = " " 
+                    print(self.station_name)
+                    print(self.current_track)
+                    # for i in range(13):
+                    #     print("{} - {}".format(i, self.media.get_meta(i)))
+                time.sleep(3)
             elif self.state == 1: #start playing
                 print("Playing stream")
-                self.media.set_mrl(self.url)
-                self.media.play()
+                self.media=self.instance.media_new(self.url)
+                self.media.get_mrl()
+                self.media_player.set_media(self.media)
+                self.media_player.play()
+
                 self.state = 0
             elif self.state == 2: #stop playing
-                self.media.stop()
+                self.media_player.stop()
                 print ("Stop player!")
                 self.state = 0
     def play(self, url):
@@ -89,6 +109,10 @@ def bluetooth_connect():
     print ("Connecting to bluetooth speaker")
     # btooth.connect("00:18:09:6A:2A:D5")
     return "OK"
+
+@app.route("/get_artist")
+def get_artist():
+    return '{"station_name" : "'+ player.station_name + '", "current_track" : "' + player.current_track + '"}', 200
 
 if __name__ == "__main__":
     player.thread.start()
